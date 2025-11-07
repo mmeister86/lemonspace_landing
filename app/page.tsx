d4b18d66c74ec7ba1c2360581f5c922d748c26ed
Matthias Meister <meister.matthias86@gmail.com>
2025-11-07 17:34:07 +0100

    Navbar-Komponente hinzugefügt und Logo aktualisiert
    - Neue Navbar-Komponente mit responsivem Menü erstellt
    - Logo-Komponente mit SVG-Logo aktualisiert
    - Hero-Section angepasst
    - Logo-SVG-Datei hinzugefügt
    - Navbar in Hauptseite integriert

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import HeroSection from '@/components/hero-section'
-import FeaturesSection from '@/components/features-8'
-import FooterSection from '@/components/footer'
-import CallToAction from '@/components/call-to-action'
-import StatsSection from '@/components/stats'
-import PricingSection from '@/components/pricing'
+import React from "react";
+import Navbar from "@/components/navbar";
+import HeroSection from "@/components/hero-section";
+import FeaturesSection from "@/components/features-8";
+import FooterSection from "@/components/footer";
+import CallToAction from "@/components/call-to-action";
+import StatsSection from "@/components/stats";
+import PricingSection from "@/components/pricing";
 
 const page = () => {
   return (
     <>
-     <HeroSection />  
+      <Navbar />
+      <HeroSection />
 
-     <FeaturesSection />
-     <StatsSection />
-     <PricingSection />
-     <CallToAction />
-     <FooterSection />
+      <FeaturesSection />
+      <StatsSection />
+      <PricingSection />
+      <CallToAction />
+      <FooterSection />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
